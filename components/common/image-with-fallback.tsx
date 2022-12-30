1f6186221379a4a49f8e3e97317dbfc16856a645
Hai Tran <tranngochai171@gmail.com>
2022-12-30 21:43:50 +0700

    fix: fix build fail

--- components/common/image-with-fallback.tsx
+++ components/common/image-with-fallback.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
-import DefaultImage from '../../public/fallback-image.png';
 
 type ImageWithFallbackProps = {
   src: string;
@@ -8,15 +7,17 @@ type ImageWithFallbackProps = {
   [x: string]: any;
 };
 
+const defaultImageSrc = '/fallback-image.png';
+
 const ImageWithFallback = (
   props: ImageWithFallbackProps & React.ComponentProps<typeof Image>,
 ) => {
   const { src, isStatic, ...rest } = props;
-  const [imgSrc, setImgSrc] = useState(DefaultImage);
+  const [imgSrc, setImgSrc] = useState(defaultImageSrc);
 
   useEffect(() => {
     if (src !== imgSrc) {
-      let tempSrc = src ?? DefaultImage;
+      let tempSrc = src ?? defaultImageSrc;
       if (typeof src === 'string' && !isStatic) {
         tempSrc = src;
       }
@@ -25,16 +26,15 @@ const ImageWithFallback = (
   }, [imgSrc, isStatic, src]);
 
   return (
+    // eslint-disable-next-line jsx-a11y/alt-text
     <Image
-      src={imgSrc || DefaultImage}
-      // ts-ignore
-      alt='Supermomos'
+      src={imgSrc || defaultImageSrc}
       height={10}
       width={50}
       style={{ objectFit: 'contain' }}
       {...rest}
       onError={() => {
-        setImgSrc(DefaultImage);
+        setImgSrc(defaultImageSrc);
       }}
     />
   );
